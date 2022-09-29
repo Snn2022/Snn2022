b108d8dcf71967d51e95a7d609adbd5e9d7d16f6
nasir <fmiexclusive@gmail.com>
2022-09-30 00:52:04 +0600

    frontend updating

--- resources/views/templates/menu.blade.php
+++ resources/views/templates/menu.blade.php
@@ -29,7 +29,7 @@
                  <li class="nav-item">
                      <a href="{{route('dashboard')}}" class="nav-link active">
                          <i class="nav-icon fas fa-tachometer-alt"></i>
-                         <p>Dashboard</p>
+                         <p>সদস্যগণ</p>
 
                      </a>
                  </li>
